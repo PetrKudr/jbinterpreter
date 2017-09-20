08a954f185141d69195d834eb44c337e025058d4
Petr <petrkgm@gmail.com>
2017-09-21 02:00:24 +0300

    Dummy Interpreter

---
+++ src/main/java/ru/spb/petrk/interpreter/Interpreter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.spb.petrk.interpreter;
+
+/**
+ *
+ * @author petrk
+ */
+public class Interpreter {
+    
+    private final String input;
+
+    public Interpreter(String input) {
+        this.input = input;
+    }
+    
+    
+    
+}
